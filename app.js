9c0ca38ad3ad67a26f918ea33725d4f6ad20696a
Gabriel Barreto <gh.barreto@hotmail.com>
2021-12-03 20:13:02 -0300

    cleanup

---
+++ app.js
@@ -0,0 +1,48 @@
+const express = require('express'),
+  request = require('request'),
+  app = express(),
+  PORT = 8000;
+
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+
+let options = {
+  url: 'https://api.github.com/users/ghbarreto/repos',
+  headers: {
+    'User-Agent': 'request',
+  },
+};
+
+let optionsB = {
+  url: 'https://api.github.com/users/ghbarreto',
+  headers: {
+    'User-Agent': 'request',
+  },
+};
+app.get('/', (req, res) => {
+  let finishCount = 2;
+  let dataA, dataB;
+
+  const done = () => {
+    if (--finishCount) {
+      return;
+    }
+    res.render('index', { dataA, dataB });
+  };
+  request(options, (err, response, body) => {
+    let info = JSON.parse(body);
+    dataA = info;
+    console.log('DATA A : ' + info[0]);
+    done();
+  });
+
+  request(optionsB, (err, response, body) => {
+    let info = JSON.parse(body);
+    dataB = info;
+    done();
+  });
+});
+
+app.listen(PORT, () => {
+  console.log('Server listening on port: ' + PORT);
+});
